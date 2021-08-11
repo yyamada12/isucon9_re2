c32cbb2a8798a1ed78633bfc0cbe03d0f20b17e3
yyamada12 <12yacropolisy@gmail.com>
2021-08-11 01:04:31 +0000

    add pprof

--- isucari/webapp/go/main.go
+++ isucari/webapp/go/main.go
@@ -21,6 +21,8 @@ import (
 	goji "goji.io"
 	"goji.io/pat"
 	"golang.org/x/crypto/bcrypt"
+
+	_ "net/http/pprof"
 )
 
 const (
@@ -287,6 +289,11 @@ func init() {
 }
 
 func main() {
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	host := os.Getenv("MYSQL_HOST")
 	if host == "" {
 		host = "127.0.0.1"
