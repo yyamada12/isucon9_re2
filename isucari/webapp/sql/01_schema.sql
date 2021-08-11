a784e5b53b3bc89f877ac2fe817d8aebfbc0fc02
yyamada12 <12yacropolisy@gmail.com>
2021-08-11 08:51:31 +0000

    add index seller_id to items [10560]

--- isucari/webapp/sql/01_schema.sql
+++ isucari/webapp/sql/01_schema.sql
@@ -31,6 +31,7 @@ CREATE TABLE `items` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_category_id (`category_id`),
+  INDEX idx_seller_id (`seller_id`),
   INDEX idx_created_at_id (`created_at`,`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
